f79166204daf3b9abce0cf80177d45bd75b22f05
theubaidistan <ubaidistan395@gmail.com>
2025-11-07 21:29:29 +0500

    new changes

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -2,6 +2,7 @@ import {Inngest} from "inngest";
 
 export const inngest=new Inngest({
   id:'signalist',
+  signingKey: process.env.INNGEST_SIGNING_KEY,
   ai:{
     gemini:{
       apikey:process.env.GEMINI_API_KEY!,
